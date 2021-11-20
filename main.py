ada7ba13646c7294271fc466c399bcb3cd1ab43e
datewat <thomaszimmer524@gmail.com>
2021-11-20 10:33:10 +0100

    documentation
    changed documentation to be more specific

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import matelight.draw as draw #we need to import the draw.py for the program. We use this here as draw
+import matelight.draw as draw #we need to import the draw.py for the program, to draw shapes, here as draw.
 import ml_simulator.matelight as ml #We need to import the Matelightsimulator for testing. We use this here as ml.
-import time #we need to import time for the program
-import numpy as np #we also need the package numpy which we use as np
+import time # time is needed, to display changes, so the effect doesn't just disappear when done.
+import numpy as np # numpy is needed for some math operations.
 
 
-disp = ml.Matelight(3,4) #here the number of mate boxes is defined
+disp = ml.Matelight(3,4) #here the number of mate boxes is defined. (1 Box is 5x4, so here you get 15x16)
 
 disp.set_pixel(2,5,255,0,0) #here a point is set at x2 and at y5. In addition, the color can be changed here, which is blue by default.
-disp.show() #this line is necessary for the matelight to output something
+disp.show() #this line is necessary for the matelight to display changes.
 time.sleep(10) #this is optional, it serves to make the effect visible for a few seconds
